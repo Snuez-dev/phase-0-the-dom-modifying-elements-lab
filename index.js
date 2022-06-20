3deb80e09ed4d744ac7ec14bc694de250684a1ec
Lazaro Buron <lazaro.buron@gmail.com>
2022-06-19 20:37:15 -0400

    commit tree 80b48198ab69527be29da3b5c523062ae9331dfc

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById('main').remove(); //remove elements from node
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = "RAMLA is the champion"
